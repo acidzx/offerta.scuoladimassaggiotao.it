8abf627651760e750ef557b457d413cedade4393
acidzx <acidxtech@gmail.com>
2023-05-19 18:00:06 +0200

    Update index.js

--- landing/src/pages/index.js
+++ landing/src/pages/index.js
@@ -21,7 +21,29 @@ export default function Home(props) {
     <>
       <Header />
       <AltaFormazione />
+      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+      <div className="container mx-auto p-4 mt-6">
+        <h2 className="text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-600 md:text-3xl lg:text-4xl dark:text-white">
+          DIPLOMI DI SPECIALIZZAZIONE
+        </h2>
+        <p className="text-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+          Il Diploma di Alta Formazione Professionale per massaggiatori è il
+          percorso formativo più completo che puoi desiderare ed ottenere,
+          garantendoti un sicuro inserimento nel mondo del lavoro.
+        </p>
+      </div>
       <MultiCarousel posts={props.diplomi} />
+      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+      <div className="container mx-auto p-4 mt-6">
+        <h2 className="text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-600 md:text-3xl lg:text-4xl dark:text-white">
+          CORSI INDIVIDUALI
+        </h2>
+        <p className="text-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+          Il Diploma di Alta Formazione Professionale per massaggiatori è il
+          percorso formativo più completo che puoi desiderare ed ottenere,
+          garantendoti un sicuro inserimento nel mondo del lavoro.
+        </p>
+      </div>
       <MultiCarousel posts={props.corsi} />
     </>
   );
