5df0e735e762cc300dd107ae021246939936e1c0
acidzx <124239271+acidzx@users.noreply.github.com>
2023-05-23 16:34:57 +0200

    pre form validation rule

---
+++ landing/content/diploma/diploma-operatore-estetica-olistica.md
@@ -0,0 +1,12 @@
+---
+img: "diploma-operatore-estetica-olistica.jpg"
+slug: "diploma-operatore-estetica-olistica"
+price: "249,00"
+title: "Estetica Olistica"
+description: "Al corso di massaggio crio-dinamico imparerai a sfruttare i benefici del ghiaccio sui vari distretti muscolari. Una delle principali caratteristiche di questa tecnica è la possibilità di velocizzare il ripristino dall’affaticamento muscolare. Nonostante ciò, questo tipo di tecnica si presta anche a molte altre tipologie di trattamento."
+durata: "5 w/end"
+ore: "16 ore"
+category: "diploma"
+---
+
+# Corso Anatomia
--- landing/src/components/modalForm/index.js
+++ landing/src/components/modalForm/index.js
@@ -10,13 +10,108 @@ export default function ModalForm() {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
+          <h3 className="text-lg font-bold text-center">
+            Richiedi Informazioni
           </h3>
-          <p className="py-4">
-            You&apos;ve been selected for a chance to get one year of
-            subscription to use Wikipedia for free!
+          <p className="py-4 mx-4">
+            Compila il form per ricevere subito il tuo buono sconto sui nostri
+            corsi e percorsi di formazione!
           </p>
+          <hr />
+          {/* form */}
+
+          {/* nome */}
+          <div className="form-control w-full max-w-sm mx-auto">
+            <label className="label">
+              <span className="label-text">Qual&apos;è il tuo nome?</span>
+              <span className="label-text-alt">Top Right label</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Nome..."
+              className="input input-bordered input-accent w-full max-w-sm mx-auto"
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+              <span className="label-text-alt">Bottom Right label</span>
+            </label>
+          </div>
+          {/* cognome */}
+          <div className="form-control w-full max-w-sm mx-auto">
+            <label className="label">
+              <span className="label-text">Qual&apos;è il tuo cognome?</span>
+              <span className="label-text-alt">Top Right label</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Cognome..."
+              className="input input-bordered input-accent w-full max-w-sm mx-auto"
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+              <span className="label-text-alt">Bottom Right label</span>
+            </label>
+          </div>
+          {/* email */}
+          <div className="form-control w-full max-w-sm mx-auto">
+            <label className="label">
+              <span className="label-text">Qual&apos;è la tua email?</span>
+              <span className="label-text-alt">Top Right label</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email..."
+              className="input input-bordered input-accent w-full max-w-sm"
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+              <span className="label-text-alt">Bottom Right label</span>
+            </label>
+          </div>
+          {/* telefono */}
+          <div className="form-control w-full max-w-sm mx-auto">
+            <label className="label">
+              <span className="label-text">
+                Qual&apos;è il tuo numero di cellulare?
+              </span>
+              <span className="label-text-alt">Top Right label</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Telefono..."
+              className="input input-bordered input-accent w-full max-w-sm"
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+              <span className="label-text-alt">Bottom Right label</span>
+            </label>
+          </div>
+          <div className="flex items-center my-3 mx-auto justify-center">
+            <input
+              id="checkbox-1"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="checkbox-1"
+              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              I agree to the{" "}
+              <a
+                href="#"
+                className="text-green-600 hover:underline dark:text-green-500"
+              >
+                terms and conditions
+              </a>
+              .
+            </label>
+          </div>
+          <div className="mx-auto text-center mt-5">
+            <button className="btn text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+              Invia
+            </button>
+          </div>
         </label>
       </label>
     </>
