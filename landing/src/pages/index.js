4d31043c20af9ccf65478d98c9a6c2b978f0c440
acidzx <124239271+acidzx@users.noreply.github.com>
2023-06-25 23:00:23 +0200

    Update index.js

--- landing/src/pages/index.js
+++ landing/src/pages/index.js
@@ -5,7 +5,6 @@ import ModalForm from "@components/modalForm";
 import Cta from "@components/cta";
 import ServiziTao from "@components/serviziTao";
 import Testimonials from "@/components/testimonials";
-import ServizioClienti from "@components/servizioClienti";
 import { getAllPosts } from "../../lib/posts-util";
 
 export async function getStaticProps() {
@@ -56,7 +55,7 @@ export default function Home(props) {
       <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700" />
       <Testimonials />
       <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700" />
-      <ServizioClienti />
+      
     </>
   );
 }
