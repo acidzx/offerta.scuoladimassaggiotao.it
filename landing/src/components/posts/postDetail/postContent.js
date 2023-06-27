fd68a061d780f8d8462cdfcfce61c30bbdb75e72
acidzx <acidxtech@gmail.com>
2023-06-27 15:53:33 +0200

    updated corso singolo

--- landing/src/components/posts/postDetail/postContent.js
+++ landing/src/components/posts/postDetail/postContent.js
@@ -3,28 +3,134 @@ import PostHeader from "./postHeader";
 
 export default function PostContent(props) {
   const { post } = props;
-  const imgPath = `/assets/images/${post.category}/${post.slug}/${post.img}`;
+  const imgPath = `/assets/images/${post.category}/${post.slug}`;
 
   return (
     <>
-      <div className="hero min-h-screen homebg">
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+      <PostHeader
+        img={`${imgPath}/${post.img}`}
+        title={post.title}
+        description={post.description}
+      />
+      <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700" />
+      <section className="bg-white dark:bg-gray-900">
+        <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+          <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Presentazione
+            </h2>
+            <div className="mb-4 text-justify text-base">
+              <ReactMarkdown>{post.longcontent}</ReactMarkdown>
+            </div>
+            <h3 className="mb-2 text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+              I Benefici
+            </h3>
+            <div className="mb-4 text-justify text-base">
+              <ReactMarkdown>{post.benefici}</ReactMarkdown>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mt-8">
+            <img
+              className="w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+              alt="office content 1"
+            />
+            <img
+              className="mt-4 w-full lg:mt-10 rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+              alt="office content 2"
+            />
           </div>
         </div>
+      </section>
+      <div className="container mx-auto px-4 lg:px-32">
+        <div className="my-2 collapse collapse-arrow bg-green-600/5">
+          <input type="radio" name="my-accordion-2" defaultChecked />
+          <div className="collapse-title text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+            Programma del corso di {post.title}
+          </div>
+          <div className="collapse-content p-5">
+            <ul className="ml-5 pb-3 list-disc">
+              {post.programma.map((progList, i) => (
+                <li key={i}>{progList}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="my-2 collapse collapse-arrow bg-green-600/5">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+            Requisiti
+          </div>
+          <div className="collapse-content">{post.requisiti}</div>
+        </div>
+        <div className="my-2 collapse collapse-arrow bg-green-600/5">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+            Orario e durata
+          </div>
+          <div className="collapse-content">{post.orario}</div>
+        </div>
+        <div className="my-2 collapse collapse-arrow bg-green-600/5">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+            Sedi del corso
+          </div>
+          <div className="collapse-content">
+            <p>
+              La Tao - Scuola Nazionale di Massaggio ha sedi dislocate su tutto
+              il territorio nazionale nelle maggiori città italiane.
+            </p>
+            <p>
+              Per garantire il massimo servizio all’allievo svolgiamo la
+              formazione in tutte le migliori sale congressi di prestigiosi
+              Hotel, tutte comodamente raggiungibili poiché ubicate nei pressi
+              dei principali svincoli autostradali e dotate di tutti i comfort e
+              servizi richiesti per un corretto svolgimento del corso, oltre
+              alla presenza di un videoproiettore al fine di svolgere la parte
+              di lezione teorica iniziale.
+            </p>
+            <p className="text-center">
+              Alessandria - Ancona - Arco - Bari - Bologna - Brescia - Cagliari
+              - Catania - Cesena - Chianciano Terme - Cuneo - Enna - Firenze -
+              Foggia - Genova - Lamezia Terme - Lecce - Milano - Montegrotto
+              Terme - Napoli - Novara - Palermo - Parma - Perugia - Pescara -
+              Piacenza - Pisa - Pordenone - Roma - Torino - Trento - Udine -
+              Verona
+            </p>
+          </div>
+        </div>
+        <div className="my-2 collapse collapse-arrow bg-green-600/5">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+            Servizi inclusi
+          </div>
+          <div className="collapse-content">
+            <ul className="ml-5 pb-3 list-disc">
+              <li>
+                Garanzia Tao - Soddisfatto o Rimborsato; Materiale didattico
+                gratuito;
+              </li>
+              <li>Assistenza post-formazione gratuita;</li>
+              <li>Rateizzazione dei percorsi;</li>
+              <li>Tutor personale;</li>
+              <li>Formazione sempre garantita;</li>
+              <li>Collocamento nel mondo del lavoro;</li>
+              <li>
+                Stages post-formazione e partecipazione ad eventi importanti;
+              </li>
+              <li>Accreditamento dei corsi svolti anche con altre realtà.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="my-2 collapse collapse-arrow bg-green-600/5">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-2xl tracking-tight font-extrabold text-gray-700 dark:text-white">
+            Attestazione rilasciata
+          </div>
+          <div className="collapse-content">{post.attestazione}</div>
+        </div>
       </div>
-      <article>
-        <PostHeader img={imgPath} title={post.title} />
-        <ReactMarkdown>{post.content}</ReactMarkdown>
-      </article>
     </>
   );
 }
--- landing/src/components/posts/postDetail/postHeader.js
+++ landing/src/components/posts/postDetail/postHeader.js
@@ -1,12 +1,91 @@
 import Image from "next/image";
 
+import {
+  BuildingLibraryIcon,
+  HandRaisedIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+
+const supportLinks = [
+  {
+    name: "33 sedi nazionali",
+    href: "#",
+    icon: BuildingLibraryIcon,
+  },
+  {
+    name: "40+ tecniche di massaggio",
+    href: "#",
+    icon: HandRaisedIcon,
+  },
+  {
+    name: "3500+ allievi formati ogni anno",
+    href: "#",
+    icon: UserGroupIcon,
+  },
+];
+
 export default function PostHeader(props) {
-  const { title, img } = props;
+  const { title, img, description } = props;
 
   return (
-    <header>
-      <h1>{title}</h1>
-      <Image alt={title} src={img} width={200} height={150} />
-    </header>
+    <div className="bg-white">
+      {/* Header */}
+      <div className="relative bg-gray-800 pb-32">
+        <div className="absolute inset-0">
+          <Image
+            className="h-full w-full object-cover"
+            alt={title}
+            src={img}
+            width={1750}
+            height={500}
+          />
+          <div
+            className="absolute inset-0 bg-gray-800/5 mix-blend-multiply"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="relative mx-auto max-w-7xl py-24 px-6 sm:py-32 lg:px-8">
+          <h1 className="text-4xl font-bold tracking-tight text-white md:text-5xl lg:text-6xl">
+            {title}
+          </h1>
+          <p className="mt-6 max-w-3xl text-xl text-white">{description}</p>
+        </div>
+      </div>
+
+      {/* Overlapping cards */}
+      {
+        <section
+          className="relative z-10 mx-auto -mt-[30px] max-w-7xl px-6 pb-24 lg:px-8"
+          aria-labelledby="contact-heading"
+        >
+          <h2 className="sr-only" id="contact-heading">
+            Features
+          </h2>
+          <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8">
+            {supportLinks.map((link) => (
+              <div
+                key={link.name}
+                className="flex flex-col rounded-2xl bg-white shadow-xl"
+              >
+                <div className="relative flex-1 px-6 pt-16 pb-8 md:px-8">
+                  <div className="absolute top-0 inline-block -translate-y-1/2 transform rounded-xl bg-gray-400 p-5 shadow-lg">
+                    <link.icon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <h3 className="text-xl font-medium text-gray-900">
+                    {link.name}
+                  </h3>
+                  {/*   <p className="mt-4 text-base text-gray-500">
+                    {link.description}
+                  </p> */}
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      }
+    </div>
   );
 }
