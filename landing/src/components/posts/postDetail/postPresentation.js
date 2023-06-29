ccf66ebadb1c9472aefb83eb73050f66dd64ed2a
acidzx <acidxtech@gmail.com>
2023-06-29 17:51:31 +0200

    Update postPresentation.js

--- landing/src/components/posts/postDetail/postPresentation.js
+++ landing/src/components/posts/postDetail/postPresentation.js
@@ -5,7 +5,7 @@ export default function PostPresentation(props) {
 
   return (
     <section className="bg-white ">
-      <div className="items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg">
           <h2 className="mb-4 text-4xl font-extrabold text-gray-600 ">
             Presentazione
