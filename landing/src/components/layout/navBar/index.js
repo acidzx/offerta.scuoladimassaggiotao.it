29c96adfd7b5916953e3d14d39f5352c08a8b03e
acidzx <acidxtech@gmail.com>
2023-07-07 15:01:03 +0200

    Update index.js

--- landing/src/components/layout/navBar/index.js
+++ landing/src/components/layout/navBar/index.js
@@ -3,7 +3,7 @@ import Logo from "./logo";
 
 export default function NavBar() {
   return (
-    <div className="w-full bg-gray-200">
+    /*   <div className="w-full bg-gray-200">
       <div className="container mx-auto">
         <div className="navbar bg-base-100">
           <div className="flex-1">
@@ -12,6 +12,19 @@ export default function NavBar() {
           <div className="flex-none">{""}</div>
         </div>
       </div>
-    </div>
+    </div> */
+
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <Logo />
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
