cea497b4727c571aaaf915ef1540e9b25cfc418e
acidzx <acidxtech@gmail.com>
2023-07-07 12:26:22 +0200

    Update index.js

--- landing/src/components/layout/index.js
+++ landing/src/components/layout/index.js
@@ -7,7 +7,7 @@ import { PhoneIcon } from "@heroicons/react/24/outline";
 export default function Layout({ children }) {
   return (
     <Fragment>
-      <div className="w-full bg-gray-400">
+      <div className="w-full bg-gray-400 overflow-visible">
         <div className="container mx-auto">
           <div className="flex">
             <div className="flex-1">
