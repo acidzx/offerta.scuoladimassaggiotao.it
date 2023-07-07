89846f8b619ee59ade9a61c79def43b196c975a9
acidzx <acidxtech@gmail.com>
2023-07-07 12:48:45 +0200

    Update _app.js

--- landing/src/pages/_app.js
+++ landing/src/pages/_app.js
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
     // <ThemeProvider attribute="class">
     <Layout>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </Layout>
