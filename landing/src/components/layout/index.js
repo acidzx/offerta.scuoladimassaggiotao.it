17142e1a7d345abddc05aea4fccbf8b9f51dae6e
acidzx <124239271+acidzx@users.noreply.github.com>
2023-07-10 23:44:12 +0200

    updates

--- landing/src/components/layout/index.js
+++ landing/src/components/layout/index.js
@@ -8,5 +8,5 @@ export default function Layout({ children }) {
       <main>{children}</main>
       <Footer />
     </>
-  );
+  )
 }
--- landing/src/pages/_app.js
+++ landing/src/pages/_app.js
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Layout>
       <Component {...pageProps} />
-      </Layout>
+    </Layout>
     </>
   );
 }
