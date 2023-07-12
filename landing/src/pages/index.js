ce9198e9a7174a361682e4a2c3c7d44b83bc7b89
acidzx <acidxtech@gmail.com>
2023-07-12 14:02:37 +0200

    Update index.js

--- landing/src/pages/index.js
+++ landing/src/pages/index.js
@@ -10,6 +10,16 @@ import { getAllPosts } from "../../lib/posts-util";
 import Head from "next/head";
 
 export async function getStaticProps() {
+  const res = await fetch(
+    `https://graph.facebook.com/v17.0/scuoladimassaggio/ratings?fields=reviewer%2Ccreated_time%2Creview_text&access_token=EAACeqpKifLcBABhwZAnbBASwS3ZAFDDNrSrnRDSreKUoTo9aS38m893nAoqZBrg8i0zkVumkNLhNeGwa1KVialkD2CofUaZCwNMHk7PH6CX8O2XdIsWb36hALJNKLUl3OUxjt1zI4UPvYpR8EFCDZBFgwWUuldTyahDe7XgcG8rcX5C3ZA9cJKRfNkp68H7zdCHV9OZCM0gDQZDZD`,
+    { cache: "force-cache" }
+  );
+  /* 
+  curl -i -X GET \
+ "https://graph.facebook.com/v17.0/scuoladimassaggio/ratings?fields=reviewer%2Ccreated_time%2Creview_text&access_token=EAACeqpKifLcBABhwZAnbBASwS3ZAFDDNrSrnRDSreKUoTo9aS38m893nAoqZBrg8i0zkVumkNLhNeGwa1KVialkD2CofUaZCwNMHk7PH6CX8O2XdIsWb36hALJNKLUl3OUxjt1zI4UPvYpR8EFCDZBFgwWUuldTyahDe7XgcG8rcX5C3ZA9cJKRfNkp68H7zdCHV9OZCM0gDQZDZD"
+ */
+  const reviews = await res.json();
+
   const allCorsi = getAllPosts("corso");
   const allDiplomi = getAllPosts("diploma");
 
@@ -17,6 +27,7 @@ export async function getStaticProps() {
     props: {
       corsi: allCorsi,
       diplomi: allDiplomi,
+      reviews,
     },
     // revalidate: 1800,
   };
@@ -25,6 +36,7 @@ export async function getStaticProps() {
 export default function Home(props) {
   return (
     <>
+      {console.log(props.reviews)}
       <Head>
         <title>Offerta Corsi Tao - Scuola Nazionale di Massaggio</title>
         <meta
