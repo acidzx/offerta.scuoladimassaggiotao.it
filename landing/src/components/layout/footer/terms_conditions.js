50e317d352bcc19b67fe3192e9dc4c621166ba0c
acidzx <acidxtech@gmail.com>
2023-07-13 14:31:32 +0200

    Update terms_conditions.js

--- landing/src/components/layout/footer/terms_conditions.js
+++ landing/src/components/layout/footer/terms_conditions.js
@@ -5,11 +5,15 @@ export default function TermsConditions() {
     <dialog id="termsConditions" className="modal">
       <form method="dialog" className="modal-box w-11/12 max-w-5xl bg-white">
         <h2>Termini e Condizioni</h2>
-        <embed
-          src="https://www.iubenda.com/termini-e-condizioni/98820787"
-          onerror="alert('URL invalid !!');"
-          className="w-full h-full"
-        />
+        <div>
+          <div className="aspect-w-16 aspect-h-9">
+            <iframe
+              src="https://www.iubenda.com/termini-e-condizioni/98820787"
+              title="termini e condizioni tao"
+              frameborder="0"
+            ></iframe>
+          </div>
+        </div>
         <div className="modal-action">
           {/* if there is a button, it will close the modal */}
           <button className="btn">Close</button>
