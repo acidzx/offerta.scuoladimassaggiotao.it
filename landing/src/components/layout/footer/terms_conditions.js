6c897753752a5d844f89ca0e9e88578cedad3f4e
acidzx <acidxtech@gmail.com>
2023-07-13 15:10:37 +0200

    Update terms_conditions.js

--- landing/src/components/layout/footer/terms_conditions.js
+++ landing/src/components/layout/footer/terms_conditions.js
@@ -2,15 +2,16 @@ import { Fragment } from "react";
 
 export default function TermsConditions() {
   return (
-    <dialog id="termsConditions" className="modal">
+    <dialog id="termsConditions" className="modal overflow-hidden">
       <form method="dialog" className="modal-box w-11/12 max-w-5xl bg-white">
         <h2>Termini e Condizioni</h2>
         <div>
           <div className="aspect-w-16 aspect-h-9">
             <iframe
-              src="#"
+              src="https://www.iubenda.com/termini-e-condizioni/98820787"
               title="termini e condizioni tao"
               frameborder="0"
+              className=" border-0"
             ></iframe>
           </div>
         </div>
