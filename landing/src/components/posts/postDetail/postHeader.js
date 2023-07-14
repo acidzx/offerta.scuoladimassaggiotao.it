0c04453e9f57ef955efbf91b84f4911edc5ebd27
acidzx <acidxtech@gmail.com>
2023-07-14 17:14:28 +0200

    Update postHeader.js

--- landing/src/components/posts/postDetail/postHeader.js
+++ landing/src/components/posts/postDetail/postHeader.js
@@ -25,7 +25,7 @@ const supportLinks = [
 ];
 
 export default function PostHeader(props) {
-  const { title, img, description, category } = props;
+  const { title, img, description, category, filteredDate } = props;
 
   return (
     <div className="bg-white">
@@ -87,6 +87,11 @@ export default function PostHeader(props) {
               </div>
             ))}
           </div>
+          {category === "corso" ? (
+            <div className="container text-center">{filteredDate}</div>
+          ) : (
+            ""
+          )}
         </div>
       }
     </div>
