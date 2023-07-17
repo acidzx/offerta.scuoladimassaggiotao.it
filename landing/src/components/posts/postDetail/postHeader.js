45d7515e28b0dfec1bf59a5a8a291843c07d2cab
acidzx <acidxtech@gmail.com>
2023-07-17 11:15:46 +0200

    Update postHeader.js

--- landing/src/components/posts/postDetail/postHeader.js
+++ landing/src/components/posts/postDetail/postHeader.js
@@ -58,7 +58,7 @@ export default function PostHeader(props) {
       {/* Overlapping cards */}
       {
         <div
-          className="hidden lg:block relative z-10 mx-auto lg:-mt-[30px] max-w-7xl px-6 pb-24 lg:px-8"
+          className="hidden lg:block relative z-10 mx-auto lg:-mt-[30px] max-w-7xl px-6 pb-8 lg:px-8"
           aria-labelledby="contact-heading"
         >
           <h2 className="sr-only" id="contact-heading">
