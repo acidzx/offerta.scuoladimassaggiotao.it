3cbc3c239551746cf1582111f37f96b9d026c02c
acidzx <acidxtech@gmail.com>
2023-07-18 12:39:09 +0200

    Update thank-you.js

--- landing/src/pages/thank-you.js
+++ landing/src/pages/thank-you.js
@@ -1,3 +1,57 @@
 export default function ThankYou() {
-  return <div>grazie</div>;
+  return (
+    <div className="flex items-center justify-center h-screen mx-auto">
+      <div>
+        <div className="flex flex-col items-center space-y-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="text-green-600 w-28 h-28"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <h1 className="text-4xl font-bold pb-6">
+            Grazie per il tuo interesse !
+          </h1>
+          <p className="text-center mb-3">
+            Un nostro tutor ti contatterà a breve e senza impegno per darti
+            tutte le informazioni sulla nostra offerta formativa.
+          </p>
+
+          <p className="text-center pb-8">
+            I numeri del nostro servizio clienti sono lo 0578.62772, 0578.61191
+            e 391.7405112
+          </p>
+
+          <a
+            href="/"
+            className="inline-flex items-center px-4 py-2 text-white bg-green-700 border border-green-600 rounded-full hover:bg-green-800 focus:outline-none focus:ring"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-3 h-3 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7 16l-4-4m0 0l4-4m-4 4h18"
+              />
+            </svg>
+            <span className="text-sm font-medium">Home</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
