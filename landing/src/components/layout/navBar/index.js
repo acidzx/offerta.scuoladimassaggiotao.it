9133886dbc781789adf3fba50392fb37380bb231
acidzx <acidxtech@gmail.com>
2023-07-18 13:14:55 +0200

    Update index.js

--- landing/src/components/layout/navBar/index.js
+++ landing/src/components/layout/navBar/index.js
@@ -16,7 +16,7 @@ export default function NavBar() {
           <div className="flex-none flex">
             <a
               href="tel:057862772"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 hidden md:flex"
             >
               0578.62772
               <PhoneIcon className="ml-2 -mr-1 w-5 h-5" />
