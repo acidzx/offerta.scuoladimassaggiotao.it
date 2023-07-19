4fd945b8d04da70520e2ee222d0d99ce51406ed6
acidzx <acidxtech@gmail.com>
2023-07-19 18:45:09 +0200

    Update index.js

--- landing/src/components/testimonials/youtubeTestimonial/index.js
+++ landing/src/components/testimonials/youtubeTestimonial/index.js
@@ -3,7 +3,7 @@ export default function YoutubeTestimonial() {
     <div className="w-full lg:items-center">
       <div className="aspect-w-16 aspect-h-9">
         <iframe
-          src="https://www.youtube-nocookie.com/embed/CV1VpGHIdnA"
+          src="https://www.youtube-nocookie.com/embed/aFnpKRvEX14"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="Video recensioni Tao - Scuola Nazionale di Massaggio"
