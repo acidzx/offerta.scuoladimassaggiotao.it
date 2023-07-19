6b1ccfec8a85d1e8f1dfe5645f0beda74c295a51
acidzx <acidxtech@gmail.com>
2023-07-19 18:09:38 +0200

    Update postServiziInclusi.js

--- landing/src/components/posts/postDetail/postServiziInclusi.js
+++ landing/src/components/posts/postDetail/postServiziInclusi.js
@@ -1,96 +1,39 @@
+const serviziText = [
+  {
+    name: "Garanzia Soddisfatto o",
+    subname: "Rimborsato",
+    icon: "",
+    color: "bg-green-700",
+  },
+  {
+    name: "Attestato di",
+    subname: "Specializzazione",
+    icon: "",
+    color: "bg-green-700",
+  },
+  {
+    name: "Ripasso",
+    subname: "Gratuito",
+    icon: "",
+    color: "bg-green-700",
+  },
+  {
+    name: "Materiale Didattico",
+    subname: "Incluso",
+    icon: "",
+    color: "bg-green-700",
+  },
+];
+
 export default function PostServiziInclusi(props) {
   const { title } = props;
 
   return (
-    <div className="bg-white flex flex-row flex-wrap">
-      <div className="pt-8 pb-4 px-4 mx-auto max-w-screen-xl sm:pt-8 sm:pb-16 lg:px-6">
-        <div className="hidden xl:block justify-center mx-auto">
-          <div className="stats shadow mx-auto mt-8 flex-wrap justify-center">
-            <div className="stat">
-              <div className="stat-figure text-gray-600">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-8 h-8 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  ></path>
-                </svg>
-              </div>
-              <div className="stat-title">Garanzia soddisfatto o</div>
-              <div className="stat-value text-gray-600">Rimborsato</div>
-            </div>
-            {title === "Massaggio Base Svedese" ? (
-              <>
-                <div className="stat">
-                  <div className="stat-figure text-gray-600">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      className="inline-block w-8 h-8 stroke-current"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <div className="stat-title">Video corso</div>
-                  <div className="stat-value text-gray-600">Omaggio</div>
-                </div>
-                <div className="stat">
-                  <div className="stat-figure text-gray-600">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      className="inline-block w-8 h-8 stroke-current"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <div className="stat-title">Attestato di</div>
-                  <div className="stat-value text-gray-600">
-                    Specializzazione
-                  </div>
-                </div>
-              </>
-            ) : (
-              <div className="stat">
-                <div className="stat-figure text-gray-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-8 h-8 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <div className="stat-title">Attestato di</div>
-                <div className="stat-value text-gray-600">Specializzazione</div>
-              </div>
-            )}
-
-            <div className="stat">
+    <>
+      <div className="bg-white flex justify-center my-12">
+        <div className="stats stats-vertical lg:stats-horizontal shadow">
+          {title === "Massaggio Base Svedese" ? (
+            <div className="stat lg:px-3 xl:px-4">
               <div className="stat-figure text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -106,111 +49,37 @@ export default function PostServiziInclusi(props) {
                   ></path>
                 </svg>
               </div>
-              <div className="stat-title">Ripasso</div>
-              <div className="stat-value text-gray-600">Gratuito</div>
+              <div className="stat-title">Video corso</div>
+              <div className="stat-value text-gray-600 text-xl">Omaggio</div>
             </div>
-          </div>
-        </div>
-
-        <div className="block xl:hidden">
-          <div className="container mx-auto max-w-xl text-start">
-            <div className="stats shadow mx-auto">
-              {title === "Massaggio Base Svedese" ? (
-                <div className="stat py-1">
-                  <div className="stat-figure text-gray-600">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      className="inline-block w-8 h-8 stroke-current"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <div className="stat-title">Video corso professionale</div>
-                  <div className="stat-value text-gray-600">Omaggio</div>
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
-          <div className="container mx-auto max-w-xl text-end">
-            <div className="stats shadow mx-auto mt-5">
-              <div className="stat py-1">
-                <div className="stat-figure text-gray-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-8 h-8 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <div className="stat-title">Garanzia soddisfatto o</div>
-                <div className="stat-value text-gray-600">Rimborsato</div>
+          ) : (
+            [null]
+          )}
+          {serviziText.map((servizio) => (
+            <div className="stat lg:px-3 xl:px-4" key={servizio.name}>
+              <div className="stat-figure text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 28 28"
+                  className="inline-block w-8 h-8 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="stat-title text-gray-600">{servizio.name}</div>
+              <div className="stat-value text-gray-600 text-xl">
+                {servizio.subname}
               </div>
             </div>
-          </div>
-          <div className="container mx-auto max-w-xl text-start">
-            <div className="stats shadow mx-auto mt-5">
-              <div className="stat py-1">
-                <div className="stat-figure text-gray-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-8 h-8 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
-                    ></path>
-                  </svg>
-                </div>
-                <div className="stat-title">Attestato di</div>
-                <div className="stat-value text-gray-600">Specializzazione</div>
-              </div>
-            </div>
-          </div>
-          <div className="container mx-auto max-w-xl text-end">
-            <div className="stats shadow mx-auto mt-5">
-              <div className="stat py-1">
-                <div className="stat-figure text-gray-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-8 h-8 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <div className="stat-title">Ripasso</div>
-                <div className="stat-value text-gray-600">Gratuito</div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
 }
