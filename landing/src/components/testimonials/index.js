5168f33df75beb7853d8b8fc075e0ac24622f1d3
acidzx <acidxtech@gmail.com>
2023-07-19 11:40:09 +0200

    Update index.js

--- landing/src/components/testimonials/index.js
+++ landing/src/components/testimonials/index.js
@@ -17,7 +17,7 @@ export default function Testimonials(props) {
       <div className="overflow-hidden bg-white">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid justify-center items-center max-w-2xl grid-cols-1 gap-x-8 gap-y-4 sm:gap-y-2 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div className="carousel w-full items-center">
+            <div className="carousel w-full lg:items-center">
               {FBreviews &&
                 FBreviews.data
                   .filter((reviewTextLength) => {
