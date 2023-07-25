d8f40a3d326c24008c18f340b0bd280c1db3e2e5
acidzx <acidxtech@gmail.com>
2023-07-25 16:55:40 +0200

    Update _app.js

--- landing/src/pages/_app.js
+++ landing/src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 
 import { motion } from "framer-motion";
-import { AppProps } from "next/app";
+// import { AppProps } from "next/app";
 
 // import { ThemeProvider } from "next-themes";
 
@@ -9,10 +9,6 @@ import { useEffect } from "react";
 import analytics from "@/utility/analytics";
 import Head from "next/head";
 import Layout from "@components/layout";
-
-// import PrivacyPolicy from "@components/layout/footer/privacyPolicy";
-// import TermsConditions from "@components/layout/footer/terms_conditions";
-// import CookiePolicy from "@components/layout/footer/cookiePolicy";
 
 export const scrollIntoTheView = (id) => {
   if (typeof window !== "undefined") {
@@ -40,25 +36,21 @@ export default function App({ Component, pageProps, router }) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Layout>
-        <motion.div
-          key={router.route}
-          initial="initial"
-          animate="animate"
-          variants={{
-            initial: {
-              opacity: 0,
-            },
-            animate: { opacity: 1 },
-          }}
-        >
+      <motion.div
+        key={router.route}
+        initial="initial"
+        animate="animate"
+        variants={{
+          initial: {
+            opacity: 0,
+          },
+          animate: { opacity: 1 },
+        }}
+      >
+        <Layout>
           <Component {...pageProps} />
-        </motion.div>
-      </Layout>
-
-      {/* <PrivacyPolicy />
-      <CookiePolicy />
-      <TermsConditions /> */}
+        </Layout>
+      </motion.div>
     </>
   );
 }
