bb25c56d000fa0ac98109f2e63c1bfe7ac68bbfc
acidzx <acidxtech@gmail.com>
2023-07-26 15:42:39 +0200

    Update _app.js

--- landing/src/pages/_app.js
+++ landing/src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
-import { motion } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
+
 // import { AppProps } from "next/app";
 
 // import { ThemeProvider } from "next-themes";
@@ -30,13 +31,23 @@ export default function App({ Component, pageProps, router }) {
     // this will fire the Page Track function on every new router change.
   }, []);
 
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
     <>
       <Head>
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-
+      <motion.div
+        className="h-2 origin-[0] z-50 fixed top-0 left-0 right-0 bg-green-700"
+        style={{ scaleX }}
+      />
       <Layout>
         <motion.div
           key={router.route}
